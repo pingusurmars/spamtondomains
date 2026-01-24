2ff1b8b03c9c54445970ae3f85f615e137a491e1
PinguSurMars_ <pingusurmars@proton.me>
2026-01-24 12:53:11 +0100

    Initial commit: Spamton Domains [BIG SHOT] Edition

---
+++ script.js
@@ -0,0 +1,17 @@
+/**
+ * SPAMTON DOMAINS - Functional Logic
+ * (No animations as per request)
+ */
+
+function handleBuy() {
+    console.log("Purchase intent: cgcristi.xyz");
+    alert("NOW'S YOUR CHANCE TO BE A [BIG SHOT]!\n\nTo complete your purchase of cgcristi.xyz for 9,999 KROMER, please contact our sales office at: 1-800-BIG-SHOT\n\nSales rep: SPAMTON G. SPAMTON");
+}
+
+function handleOffer() {
+    console.log("Negotiation intent: cgcristi.xyz");
+    const offer = prompt("WANT TO MAKE ME A [SPECIL DEAL]?\n\nENTER YOUR OFFER IN KROMER:");
+    if (offer) {
+        alert("W0W! THAT'S A [[High Quality]] OFFER!\n\nWE'LL GET BACK TO YOU AFTER THE [[Hyperlink Blocked]] IS REMOVED.");
+    }
+}
